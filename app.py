2f432e196b1b51ead059979e2d82199970800201
TheOrbMaverick <enangita@gmail.com>
2024-04-27 00:41:03 +0100

    threaded

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
+"""
+Using flask framework for building
+"""
 
 app = Flask(__name__)
+"""Assigning app to Flask"""
 
 
 app.route("/")
-
-
 def hello_world():
+    """ Return Hello World """
     return "<p>Hello World!</p>"
 
 
 if __name__ == "__main__":
+    """ If script name is main """
     app.run(debug=True)
