111045a642c3e5ead49296c82ece0dc917f92557
TheOrbMaverick <enangita@gmail.com>
2024-04-27 23:11:02 +0100

    what?

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,5 +2,4 @@
 """create blueprint"""
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route("/")
-@app_views.route("/status", methods=['GET'])
+@app_views.route("/status", methods=["GET"])
 def status():
     """ status page """
     return jsonify({"status": "OK"})
