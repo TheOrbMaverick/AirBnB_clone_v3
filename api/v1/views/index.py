ecadc3c73a57178147e78f3735b2ac36b9f05803
TheOrbMaverick <enangita@gmail.com>
2024-04-28 00:24:35 +0100

    what?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 """ Doing the index for flask """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
 @app_views.route("/status", methods=["GET"])
 def status():
-    """ status page """
+    """ Status page """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    number_of = {}
+    for cls in classes:
+        number_of[cls] = storage.count(classes[cls])
+    """ Return count """
+    return jsonify(number_of)
