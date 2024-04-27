46b4da327b10c49964506e94315c8fc73399dfeb
TheOrbMaverick <enangita@gmail.com>
2024-04-27 19:54:59 +0100

    master branch not working

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Using flask Blueprint """
 from flask import Blueprint
-from api.v1.views.index import *
 
 """Create a Blueprint instance with a URL prefix of '/api/v1'"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
