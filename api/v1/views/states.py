bf70b2a711bdcc8ea70a370a24ad231ab9ed9da8
TheOrbMaverick <enangita@gmail.com>
2024-04-28 21:38:17 +0100

    re-use

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """State class"""
-from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
-from models import storage, State
+from flask import jsonify, abort, request
+from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'])
