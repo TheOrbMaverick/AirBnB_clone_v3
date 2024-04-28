f1571076c644e7a0b951df87a0d5cd259b2a3fdf
TheOrbMaverick <enangita@gmail.com>
2024-04-28 23:40:01 +0100

    re-city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,7 +6,7 @@ from models import storage
 from models.city import City
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'])
+@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['GET'])
 def get_state_cities(state_id):
     """Retrieves the list of all City objects of a State"""
     state = storage.get(City, state_id)
@@ -16,7 +16,7 @@ def get_state_cities(state_id):
     return jsonify(cities)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'])
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['GET'])
 def get_city(city_id):
     """Retrieves a City object"""
     city = storage.get(City, city_id)
@@ -25,7 +25,7 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'])
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['DELETE'])
 def delete_city(city_id):
     """Deletes a City object"""
     city = storage.get(City, city_id)
@@ -36,7 +36,7 @@ def delete_city(city_id):
     return jsonify({})
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'])
+@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['POST'])
 def create_city(state_id):
     """Creates a City"""
     state = storage.get(City, state_id)
@@ -53,7 +53,7 @@ def create_city(state_id):
     return jsonify(city.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'])
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['PUT'])
 def update_city(city_id):
     """Updates a City object"""
     city = storage.get(City, city_id)
