0b9622b88bd08a0af77fa9af3fc5e19a08dc0181
TheOrbMaverick <enangita@gmail.com>
2024-04-28 19:52:38 +0100

    whew

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,23 @@ from models import storage
 
 
 @app_views.route('/states/', methods=['GET'])
-def list_states():
+def list_all_states():
     '''Retrieves a list of all State objects'''
-    list_states = [obj.to_dict() for obj in storage.all("State").values()]
+    list_states = []
+    for obj in storage.all("State").keys():
+        obj_dict = obj.to_dict()
+        list_states.append(obj_dict)
     return jsonify(list_states)
+
+
+@app_views.route('/states/<states_id>', methods=['GET'])
+def list_state_by_id(state_id):
+    list_states = []
+    state_id
+    for obj in storage.all("State").values():
+        if obj.id == state_id:
+            obj_dict = obj.to_dict()
+            list_states.append(obj_dict)
+        else:
+            abort(404)
+        
