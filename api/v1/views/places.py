deab53e06cab1c9a79f00bbfdd834e2ed9fb3f3f
TheOrbMaverick <enangita@gmail.com>
2024-04-29 19:08:56 +0100

    users

---
+++ api/v1/views/places.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""places"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.place import Place
+from datetime import datetime
+import uuid
+
+
+@app_views.route('/places/', methods=['GET'])
+def list_places():
+    '''Retrieves a list of all Place objects'''
+    list_stored_places = [obj.to_dict() for obj in storage.all("Place").values()]
+    return jsonify(list_stored_places)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'])
+def get_place(place_id):
+    '''Retrieves an Place object'''
+    all_stored_places = storage.all("Place").values()
+    one_place_obj = [obj.to_dict() for obj in all_stored_places
+                   if obj.id == place_id]
+    if one_place_obj == []:
+        abort(404)
+    return jsonify(one_place_obj[0])
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'])
+def delete_place(place_id):
+    '''Deletes an Place object'''
+    all_stored_places = storage.all("Place").values()
+    one_place_obj = [obj.to_dict() for obj in all_stored_places
+                   if obj.id == place_id]
+    if one_place_obj == []:
+        abort(404)
+    one_place_obj.remove(one_place_obj[0])
+    for obj in all_stored_places:
+        if obj.id == place_id:
+            storage.delete(obj)
+            storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/', methods=['POST'])
+def create_place():
+    '''Creates an Place'''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    places = []
+    new_place = Place(name=request.json['name'])
+    storage.new(new_place)
+    storage.save()
+    places.append(new_place.to_dict())
+    return jsonify(places[0]), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'])
+def updates_place(place_id):
+    '''Updates an Place object'''
+    all_stored_places = storage.all("Place").values()
+    one_place_obj = [obj.to_dict() for obj in all_stored_places
+                   if obj.id == place_id]
+    if one_place_obj == []:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    one_place_obj[0]['name'] = request.json['name']
+    for obj in all_stored_places:
+        if obj.id == place_id:
+            obj.name = request.json['name']
+    storage.save()
+    return jsonify(one_place_obj[0]), 200
---
+++ api/v1/views/users.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""users"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+from datetime import datetime
+import uuid
+
+
+@app_views.route('/users/', methods=['GET'])
+def list_users():
+    '''Retrieves a list of all User objects'''
+    list_stored_users = [obj.to_dict() for obj in storage.all("User").values()]
+    return jsonify(list_stored_users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    '''Retrieves an User object'''
+    all_stored_users = storage.all("User").values()
+    one_user_obj = [obj.to_dict() for obj in all_stored_users
+                   if obj.id == user_id]
+    if one_user_obj == []:
+        abort(404)
+    return jsonify(one_user_obj[0])
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    '''Deletes an User object'''
+    all_stored_users = storage.all("User").values()
+    one_user_obj = [obj.to_dict() for obj in all_stored_users
+                   if obj.id == user_id]
+    if one_user_obj == []:
+        abort(404)
+    one_user_obj.remove(one_user_obj[0])
+    for obj in all_stored_users:
+        if obj.id == user_id:
+            storage.delete(obj)
+            storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/users/', methods=['POST'])
+def create_user():
+    '''Creates an User'''
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    users = []
+    new_user = User(name=request.json['name'])
+    storage.new(new_user)
+    storage.save()
+    users.append(new_user.to_dict())
+    return jsonify(users[0]), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def updates_user(user_id):
+    '''Updates an User object'''
+    all_stored_users = storage.all("User").values()
+    one_user_obj = [obj.to_dict() for obj in all_stored_users
+                   if obj.id == user_id]
+    if one_user_obj == []:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    one_user_obj[0]['name'] = request.json['name']
+    for obj in all_stored_users:
+        if obj.id == user_id:
+            obj.name = request.json['name']
+    storage.save()
+    return jsonify(one_user_obj[0]), 200
