2ccd8684757edd2695d416b03971b4629d6eec1a
TheOrbMaverick <enangita@gmail.com>
2024-04-29 21:22:48 +0100

    tired

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,7 @@ from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.state import State
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
@@ -80,3 +81,51 @@ def update_place(place_id):
         setattr(place, key, value)
     place.save()
     return jsonify(place.to_dict()), 200
+
+@app_views.route('/places_search', methods=['POST'])
+def search_places():
+    """Search for places based on JSON parameters"""
+    try:
+        search_params = request.get_json()
+    except Exception:
+        abort(400, "Not a JSON")
+
+    if search_params is None:
+        return jsonify([place.to_dict() for place in storage.all(Place).values()])
+
+    states = search_params.get('states', [])
+    cities = search_params.get('cities', [])
+    amenities = search_params.get('amenities', [])
+
+    places = []
+    for state_id in states:
+        state = storage.get(State, state_id)
+        if state:
+            places.extend(state.places)
+
+    for city_id in cities:
+        city = storage.get(City, city_id)
+        if city:
+            places.extend(city.places)
+
+    if not states and not cities:
+        places.extend(storage.all(Place).values())
+
+    if amenities:
+        places_with_amenities = []
+        for place in places:
+            place_amenities = [amenity.id for amenity in place.amenities]
+            if all(amenity_id in place_amenities for amenity_id in amenities):
+                places_with_amenities.append(place)
+        places = places_with_amenities
+
+    return jsonify([place.to_dict() for place in places])
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'])
+def get_city_places(city_id):
+    """Retrieves the list of all Place objects of a City"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify([place.to_dict() for place in city.places])
