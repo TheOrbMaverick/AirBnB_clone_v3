f50b9bd926e10e871bdd676ef00004649be1cd21
TheOrbMaverick <enangita@gmail.com>
2024-05-01 19:19:52 +0100

    what?

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -7,9 +8,8 @@ import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-
 class User(BaseModel, Base):
-    """Representation of a user """
+    """Representation of a user"""
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -25,5 +25,18 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """Initializes user"""
+        if kwargs.get('password'):
+            kwargs['password'] = self.hash_password(kwargs['password'])
         super().__init__(*args, **kwargs)
+
+    def hash_password(self, password):
+        """Hashes the password using MD5."""
+        return hashlib.md5(password.encode()).hexdigest()
+
+    def to_dict(self, include_password=False):
+        """Returns a dictionary containing all keys/values of the instance."""
+        new_dict = super().to_dict()
+        if not include_password and 'password' in new_dict:
+            del new_dict['password']
+        return new_dict
